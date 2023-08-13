1e8a4525c93141c1ad6cc3888c0869b63c06e6b6
TrevorMann <Trevor.T.Mann@gmail.com>
2023-08-13 09:44:50 -0600

    moving signal to class

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import pandas as pd
 # Import necessary functions from other files
-from signals import (
-    compute_macd, compute_bollinger_bands, compute_rsi, compute_vwap, compute_dca, generate_vwap_signals, generate_dca_signals,
-    generate_macd_signals, generate_bollinger_signals, generate_rsi_signals, generate_combined_signals
-)
+from signals import Signals
 from backtest import trading_simulation, generate_all_signals, test_combinations
 from pricedata import fetch_kucoin_data
 
 
 def main():
     symbols = ['ETH-USDT', 'BTC-USDT']
+
     for symbol in symbols:
         df = fetch_kucoin_data(symbol)
 
+        signal = Signals(df)
+
         # Compute Indicators
-        df['macd'], df['signal'] = compute_macd(df)
-        df['upper_band'], df['lower_band'] = compute_bollinger_bands(df)
-        df['rsi'] = compute_rsi(df)
-        df['vwap'] = compute_vwap(df)
-        df['dca'] = compute_dca(df)
+        df['macd'], df['signal'] = signal.compute_macd(df)
+        df['upper_band'], df['lower_band'] = signal.compute_bollinger_bands(df)
+        df['rsi'] = signal.compute_rsi(df)
+        df['vwap'] = signal.compute_vwap(df)
+        df['dca'] = signal.compute_dca(df, 24)
 
         # Generate Signals
         df = generate_all_signals(df)
